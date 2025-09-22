b0e71ea9243ecab94d73cbef5db6b3016713b157
NoelDNathan <NoelDNathan@users.noreply.github.com>
2025-09-22 17:42:16 +0000

    deploy: e36c3698d0ae17f7c1336c97de4fa1d59c14001d

---
+++ pkg/wasm_client_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const get_video: (a: number, b: number) => any;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure98_externref_shim: (a: number, b: number, c: any) => void;
+export const closure145_externref_shim: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h3103b192acd52cdd: (a: number, b: number) => void;
+export const closure171_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_start: () => void;
