a68dbc2fdc1725af23489a0baab4d4f1b0f9cccc
NoelDNathan <noel.nathan.planell@estudiantat.upc.edu>
2026-01-13 13:37:42 +0100

    fada

--- shared-protocol/src/lib.rs
+++ shared-protocol/src/lib.rs
@@ -39,7 +39,7 @@ pub enum SignalEnum {
     // Existing messages (keep for compatibility)
     NewUser(UserID),
     TextMessage(Vec<u8>, SessionID),
-    SessionNew,
+    SessionNew(SessionID),
     SessionReady(SessionID),
     SessionJoin(SessionID),
     SessionJoinSuccess(SessionID),
